b678b0feaaa24973667cd6ccaef1f02e473e2f03
Chaha1n <Chahaaaaan@gmail.com>
2020-03-26 01:06:16 +0900

    LINEを追加

--- index.js
+++ index.js
@@ -1,53 +1,43 @@
-var app = require('./app.js');
-var Twit = require('twit');
-var CronJob = require('cron').CronJob;
-var fs = require('fs');
-var Twitter = new Twit({
+const app = require('./app.js');
+const Twit = require('twit');
+const CronJob = require('cron').CronJob;
+const fs = require('fs');
+const line = require('@line/bot-sdk');
+const send = require('./send.js');
+const Twitter = new Twit({
     consumer_key: app.get('options').key,
     consumer_secret: app.get('options').secret,
     access_token: app.get('options').token,
     access_token_secret: app.get('options').token_secret
 })
-
-var cronTime = "0 * * * * *";
+const LINE = new line.Client({
+    channelAccessToken: process.env.LINE_ACCESS_TOKEN
+});
+const cronTime = "0 * * * * *";
 
 new CronJob({
     cronTime: cronTime,
     onTick: function () {
-        tweet();
+        send(Twitter,LINE);
     },
     start: true
 });
 
-function isExist(path) {
-    let fs = require('fs');
-    try {
-        fs.statSync(path);
-        return true;
-    }
-    catch (error) {
-        if (error.code == "ENOENT")
-            return false;
-    }
-}
-function tweet() {
-    require('./DetectChanges.js')();
-    if(isExist('./last.json')) {
-        var now = JSON.parse(fs.readFileSync('./now.json', 'utf8'));
-        var last = JSON.parse(fs.readFileSync('./last.json', 'utf8'));
-        console.log(now, last);
-        console.log(now.title, last.title);
-        if (now.title !== last.title) {
-            var message = "「 "+now.title + " 」\n詳しくはこちら\n" + now.link;
-            console.log(message)
-            Twitter.post('statuses/update', {status: message}, function (err, data, response) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-        } else {
-            console.log("Nothing to tweet now.");
+const fs = require('fs');
+server.post('/bot/webhook', line.middleware(line_config), (req, res, next) => {
+    res.sendStatus(200);
+    console.log(req.body);
+    req.body.events.forEach((event) => {
+        // 参加イベント発生時にグループIDを記録
+        if (event.type == "join") {
+            const tmp = event.source.groupId;
+            console.log("This bot joined :" + groupId);
+            if (!fs.existsSync('./groups.csv')) {
+                fs.writeFileSync('./groups.csv', groupid);
+            }
+            else {
+                fs.appendFileSync('./groups.csv',  ','+groupid);
+            }
         }
-        fs.writeFileSync('./last.json', JSON.stringify(now));
-    }
-}
+    });
+});
