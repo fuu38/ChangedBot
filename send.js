1627f90af8596b3ec7095f139f29fbd0603979cc
Chaha1n <Chahaaaaan@gmail.com>
2020-03-26 01:02:08 +0900

    LINEの送信関数を作成し、Twitterと一緒にsend.jsに統合

---
+++ send.js
@@ -0,0 +1,41 @@
+function send(Twitter, LINE) {
+    const fs = require('fs');
+    const csv = require('csv');
+    require('./DetectChanges.js')();
+    if (isExist('./last.json')) {
+        var now = JSON.parse(fs.readFileSync('./now.json', 'utf8'));
+        var last = JSON.parse(fs.readFileSync('./last.json', 'utf8'));
+        console.log(now, last);
+        console.log(now.title, last.title);
+        if (now.title !== last.title) {
+            //まずTwitterに投稿
+            var message = "「 " + now.title + " 」\n詳しくはこちら\n" + now.link;
+            console.log(message)
+            Twitter.post('statuses/update', { status: message }, function (err, data, response) {
+                if (err) {
+                    console.log(err);
+                }
+            });
+            const options = {
+                type: 'text',
+                text: message
+            };
+            const groups = fs.readFileSync('./groups.csv').pipe(csv.parse());
+            groups.forEach((id) => {
+                client.pushMessage(id, options)
+                    .then(() => {
+                    })
+                    .catch((err) => {
+                        console.log(err);
+                    });
+            })
+        } else {
+            console.log("Nothing to tweet now.");
+        }
+        fs.writeFileSync('./last.json', JSON.stringify(now));
+    }
+}
+
+module.exports = {
+    send
+}
