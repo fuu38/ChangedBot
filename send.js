b088ad59d87a767df8c7fe0dc57f9ca5408b8766
Chaha1n <Chahaaaaan@gmail.com>
2020-03-26 11:24:34 +0900

    csvファイルの存在検知を追加

--- send.js
+++ send.js
@@ -1,25 +1,37 @@
-function send(Twitter, LINE) {
+module.exports = function () {
     const fs = require('fs');
     const csv = require('csv');
+    const line = require('@line/bot-sdk');
+    const line_config = {
+        channelAccessToken: process.env.LINE_ACCESS_TOKEN,
+        channelSecret: process.env.LINE_SECRET_KEY
+    };
+    const LINE = new line.Client(line_config);
+    const Twitter = new Twit({
+        consumer_key: app.get('options').key,
+        consumer_secret: app.get('options').secret,
+        access_token: app.get('options').token,
+        access_token_secret: app.get('options').token_secret
+    });
     require('./DetectChanges.js')();
-    if (fs.existsSync('./last.json')) {
-        var now = JSON.parse(fs.readFileSync('./now.json', 'utf8'));
-        var last = JSON.parse(fs.readFileSync('./last.json', 'utf8'));
-        console.log(now, last);
-        console.log(now.title, last.title);
-        if (now.title !== last.title) {
-            //まずTwitterに投稿
-            var message = "「 " + now.title + " 」\n詳しくはこちら\n" + now.link;
-            Twitter.post('statuses/update', { status: message }, function (err, data, response) {
-                if (err) {
-                    console.log(err);
-                }
-            });
-            console.log(message)
-            const options = {
-                type: 'text',
-                text: message
-            };
+    var now = JSON.parse(fs.readFileSync('./now.json', 'utf8'));
+    var last = JSON.parse(fs.readFileSync('./last.json', 'utf8'));
+    console.log(now, last);
+    console.log(now.title, last.title);
+    if (now.title !== last.title) {
+        //まずTwitterに投稿
+        var message = "「 " + now.title + " 」\n詳しくはこちら\n" + now.link;
+        Twitter.post('statuses/update', { status: message }, function (err, data, response) {
+            if (err) {
+                console.log(err);
+            }
+        });
+        console.log(message)
+        const options = {
+            type: 'text',
+            text: message
+        };
+        if (fs.existsSync('./groups.csv')) {
             const groups = fs.readFileSync('./groups.csv').pipe(csv.parse());
             groups.forEach((id) => {
                 client.pushMessage(id, options)
@@ -28,14 +40,10 @@ function send(Twitter, LINE) {
                     .catch((err) => {
                         console.log(err);
                     });
-            })
-        } else {
-            console.log("Nothing to send now.");
+            });
         }
-        fs.writeFileSync('./last.json', JSON.stringify(now));
+    } else {
+        console.log("Nothing to send now.");
     }
-}
-
-module.exports = {
-    send
+    fs.writeFileSync('./last.json', JSON.stringify(now));
 }
