46ec212fcdbb45b61283e376ac4736f73ab8d85c
Chaha1n <chahaaaaan@gmail.com>
2020-06-17 15:25:29 +0900

    健康調査を飛ばす

--- DetectChanges.js
+++ DetectChanges.js
@@ -1,17 +1,20 @@
-module.exports = function () {
+module.exports = function() {
     var cheerioCli = require('cheerio-httpcli');
     var fs = require('fs');
     cheerioCli.fetch("https://www.toyota-ct.ac.jp/information/feed", {})
-        .then(function (result) {
+        .then(function(result) {
             var TitleList = new Array();
             var LinkList = new Array();
-            result.$('title').each(function (i, element) {
+            result.$('title').each(function(i, element) {
                 TitleList.push(result.$(element).text());
             })
-            result.$('link').each(function (i, element) {
-                LinkList.push((result.$(element).text()));
-            })
-            //一番上はいらんので削除
+            result.$('link').each(function(i, element) {
+                    LinkList.push((result.$(element).text()));
+                })
+                //一番上はいらんので削除
+            TitleList.shift();
+            LinkList.shift();
+            //2020-6-17 しばらく一番上にある健康調査を飛ばす
             TitleList.shift();
             LinkList.shift();
             var json = {
@@ -23,10 +26,10 @@ module.exports = function () {
                 fs.writeFileSync('./last.json', JSON.stringify(json));
             }
         })
-        .catch(function (err) {
+        .catch(function(err) {
             console.log(err);
         })
-        .finally(function () {
+        .finally(function() {
             return { title, link };
         });
 }
